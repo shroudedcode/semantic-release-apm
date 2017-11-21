3cc186f2ab853f5ba3917a7da8b4b2eee529c664
Niklas Higi <niklas@shroudedcode.com>
2017-11-21 20:49:05 +0100

    Rename imports in `index.js`

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 const {callbackify} = require('util');
-const verifyNpm = require('./lib/verify');
-const publishNpm = require('./lib/publish');
-const getLastReleaseNpm = require('./lib/get-last-release');
+const verifyApm = require('./lib/verify');
+const publishApm = require('./lib/publish');
+const getLastReleaseApm = require('./lib/get-last-release');
 
 let verified;
 
 async function verifyConditions(pluginConfig, {pkg, logger}) {
-  await verifyNpm(pkg, logger);
+  await verifyApm(pkg, logger);
   verified = true;
 }
 
 async function getLastRelease(pluginConfig, {pkg, logger}) {
   if (!verified) {
-    await verifyNpm(pkg, logger);
+    await verifyApm(pkg, logger);
     verified = true;
   }
-  return getLastReleaseNpm(pkg, logger);
+  return getLastReleaseApm(pkg, logger);
 }
 
 async function publish(pluginConfig, {pkg, nextRelease: {version}, logger}) {
   if (!verified) {
-    await verifyNpm(pkg, logger);
+    await verifyApm(pkg, logger);
     verified = true;
   }
-  await publishNpm(version, logger);
+  await publishApm(version, logger);
 }
 
 module.exports = {
