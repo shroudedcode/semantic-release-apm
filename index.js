a53947f00269afd7ff1d48c7edc0c1e18e7cec65
Niklas Higi <niklas@shroudedcode.com>
2017-12-09 21:38:59 +0100

    Fix several bugs

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const verifyApm = require('./lib/verify');
 const publishApm = require('./lib/publish');
+const getPkg = require('./lib/get-pkg');
 const getLastReleaseApm = require('./lib/get-last-release');
 
 let verified;
 
-async function verifyConditions(pluginConfig, {pkg, logger}) {
+async function verifyConditions(pluginConfig, {logger}) {
+  const pkg = await getPkg();
   await verifyApm(pkg, logger);
   verified = true;
 }
 
-async function getLastRelease(pluginConfig, {pkg, logger}) {
+async function getLastRelease(pluginConfig, {logger}) {
+  const pkg = await getPkg();
   if (!verified) {
     await verifyApm(pkg, logger);
     verified = true;
@@ -22,7 +25,7 @@ async function publish(pluginConfig, {pkg, nextRelease, logger}) {
     await verifyApm(pkg, logger);
     verified = true;
   }
-  await publishApm(nextRelease.version, logger);
+  await publishApm(nextRelease, logger);
 }
 
 module.exports = {
